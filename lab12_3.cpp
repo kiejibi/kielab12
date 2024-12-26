3fe482d59413181fec9f379d6ffa76178e76ce40
kiejibi <kaekie2004@gmail.com>
2024-12-26 19:51:16 +0700

    add

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
-
 using namespace std;
 
-void mySwap(int &,int &); 
+void mySwap(int &,int &);
 
 int main(){
 	int x,y;
@@ -12,6 +11,11 @@ int main(){
 	mySwap(x,y);
 	cout << "After swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
-	
 	return 0;
+}
+
+void mySwap(int &a,int &b){
+	int temp = a;
+	a = b;
+	b = temp;
 }
